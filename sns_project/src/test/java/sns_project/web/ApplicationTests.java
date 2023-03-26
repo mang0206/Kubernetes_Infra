f1ee619a305be2ff6a36e33987b326ade5826417
alswosp0206 <alswosp0206@naver.com>
2023-03-26 13:29:33 +0900

    fix build.gradle and test files for build complite

--- sns_project/src/test/java/sns_project/web/ApplicationTests.java
+++ sns_project/src/test/java/sns_project/web/ApplicationTests.java
@@ -1,10 +1,10 @@
-package sns_project.demo;
+package sns_project.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests{
 
 	@Test
 	void contextLoads() {
